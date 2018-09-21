5b1fdc19aaf600159f6b69b43bfcf94007937c46
Alexander Turenko <alexander.turenko@tarantool.org>
2018-09-21 04:09:21 +0300

    merger: update test

--- test/app-tap/merger.test.lua
+++ test/app-tap/merger.test.lua
@@ -2,94 +2,249 @@
 
 local tap = require('tap')
 local buffer = require('buffer')
-local ffi = require('ffi')
 local msgpackffi = require('msgpackffi')
-local yaml = require('yaml')
 local digest = require('digest')
 local merger = require('merger')
+local crypto = require('crypto')
+local fiber = require('fiber')
 
-local function get_merger_inst()
-    local field_types = {
-        any       = 0,
-        unsigned  = 1,
-        string    = 2,
-        array     = 3,
-        number    = 4,
-        integer   = 5,
-        scalar    = 6,
-    }
+local IPROTO_DATA = 48
 
-    -- imitate parts of an index
-    local parts = {{
-        fieldno = 2,
-        type = 'unsigned',
-        is_nullable = false,
-    }}
+local schemas = {
+    {
+        name = 'small_unsigned',
+        parts = {
+            {
+                fieldno = 2,
+                type = 'unsigned',
+            }
+        },
+        gen_tuple = function(tupleno)
+            return {'id_' .. tostring(tupleno), tupleno}
+        end,
+    },
+    {
+        name = 'small_string',
+        parts = {
+            {
+                fieldno = 1,
+                type = 'string',
+            }
+        },
+        gen_tuple = function(tupleno)
+            return {'id_' .. tostring(tupleno)}
+        end,
+    },
+    {
+        name = 'huge_string',
+        parts = {
+            {
+                fieldno = 17,
+                type = 'string',
+            },
+            {
+                fieldno = 4,
+                type = 'string',
+            },
+            {
+                fieldno = 12,
+                type = 'string',
+            },
+        },
+        gen_tuple = function(tupleno)
+            local res = {}
+            for fieldno = 1, 20 do
+                res[fieldno] = crypto.digest.sha256(('%d;%d'):format(tupleno,
+                                                                     fieldno))
+            end
+            return res
+        end,
+    },
+    {
+        name = 'huge_string_eq_3',
+        parts = {
+            {
+                fieldno = 17,
+                type = 'string',
+            },
+            {
+                fieldno = 4,
+                type = 'string',
+            },
+            {
+                fieldno = 12,
+                type = 'string',
+            },
+        },
+        gen_tuple = function(tupleno)
+            local res = {}
+            for fieldno = 1, 20 do
+                if fieldno == 17 then
+                    res[fieldno] = 'field_17'
+                elseif fieldno == 4 then
+                    res[fieldno] = 'field_4'
+                else
+                    res[fieldno] = crypto.digest.sha256(('%d;%d'):format(
+                        tupleno, fieldno))
+                end
+            end
+            return res
+        end,
+    },
+}
 
-    local prepared_parts = {}
+local function get_index_fields_position(parts)
+    local res = {}
     for _, part in ipairs(parts) do
-        table.insert(prepared_parts, {
-            fieldno = part.fieldno - 1,
-            type = field_types[part.type],
-        })
+        table.insert(res, part.fieldno)
     end
-    return merger.new(prepared_parts)
+    return res
 end
 
-local function check_merger_inst(merger_inst, test, sources_count)
-    local IPROTO_DATA = 48
-    local TUPLES_CNT = 100
+local function sort_tuples(tuples, index_field_positions)
+    local function tuple_comparator(a, b)
+        for _, i in ipairs(index_field_positions) do
+            if a[i] ~= b[i] then
+                if a[i] == nil then
+                    return true
+                end
+                if b[i] == nil then
+                    return false
+                end
+                return a[i] < b[i]
+            end
+        end
+
+        return false
+    end
+
+    table.sort(tuples, tuple_comparator)
+end
+
+local function prepare_data(schema, tuples_cnt, sources_cnt, opts)
+    local opts = opts or {}
+    local use_function_input = opts.use_function_input or false
 
     local tuples = {}
-    local sorted_tuples = {}
+    local exp_result = {}
 
-    -- prepare N tables with tuples as input for merger and sorted tuples as
-    -- expected output of merger
-    for i = 1, TUPLES_CNT do
-        local id = 'id_' .. tostring(i)
-        local num = i
-        -- [1, sources_count]
-        local guava = digest.guava(i, sources_count) + 1
-        local tuple = {id, num, guava}
+    -- Prepare N tables with tuples as input for merger.
+    for i = 1, tuples_cnt do
+        -- [1, sources_cnt]
+        local guava = digest.guava(i, sources_cnt) + 1
+        local tuple = schema.gen_tuple(i)
         if tuples[guava] == nil then
             tuples[guava] = {}
         end
         table.insert(tuples[guava], tuple)
-        table.insert(sorted_tuples, tuple)
+        table.insert(exp_result, tuple)
     end
 
-    -- initialize N buffers; write corresponding tuples to that buffers;
-    -- that imitates netbox's select with {buffer = ...}
+    local index_field_positions = get_index_fields_position(schema.parts)
+
+    -- Sort prepared tuples.
+    for _, ts in pairs(tuples) do
+        sort_tuples(ts, index_field_positions)
+    end
+
+    -- Sort expected output.
+    sort_tuples(exp_result, index_field_positions)
+
+    -- Initialize N buffers; write corresponding tuples to that buffers;
+    -- that imitates netbox's select with {buffer = ...}.
     local buffers = {}
-    for i = 1, sources_count do
-        buffers[i] = buffer.ibuf()
-        msgpackffi.internal.encode_r(buffers[i], {[IPROTO_DATA] = tuples[i] or {}}, 0)
+    local inputs = {}
+    for i = 1, sources_cnt do
+        inputs[i] = buffer.ibuf()
+        msgpackffi.internal.encode_r(inputs[i],
+            {[IPROTO_DATA] = tuples[i] or {}}, 0)
+        buffers[i] = inputs[i]
     end
 
-    -- merge N buffers into res
+    -- Replace buffers[i] with a function that gives one tuple per call.
+    if use_function_input then
+        for i = 1, sources_cnt do
+            local idx = 1
+            local received_tuples
+            -- XXX: Maybe this func should be an iterator generator?
+            inputs[i] = function()
+                if received_tuples == nil then
+                    local t = msgpackffi.decode(buffers[i].buf)
+                    received_tuples = t[IPROTO_DATA]
+                end
+                local res = received_tuples[idx]
+                if res ~= nil then
+                    res = box.tuple.new(res)
+                end
+                idx = idx + 1
+                return res
+            end
+        end
+    end
+
+    return inputs, exp_result
+end
+
+local function run_merger(test, schema, tuples_cnt, sources_cnt, opts)
+    fiber.yield()
+
+    local opts = opts or {}
+    local use_function_input = opts.use_function_input or false
+
+    local inputs, exp_result =
+        prepare_data(schema, tuples_cnt, sources_cnt, opts)
+    local merger_inst = merger.new(schema.parts)
+
     local res = {}
-    merger_inst:start(buffers, 1)
+
+    -- Merge N inputs into res.
+    merger_inst:start(inputs, 1)
     while true do
         local tuple = merger_inst:next()
         if tuple == nil then break end
-        table.insert(res, tuple:totable())
+        table.insert(res, tuple)
     end
 
-    test:is_deeply(res, sorted_tuples, ('check order on %d sources'):format(
-        sources_count))
+    -- prepare for comparing
+    for i = 1, #res do
+        res[i] = res[i]:totable()
+    end
+
+    local use_function_input_str = use_function_input and
+        ' (use_function_input)' or ''
+    test:is_deeply(res, exp_result,
+        ('check order on %3d tuples in %4d sources%s')
+        :format(tuples_cnt, sources_cnt, use_function_input_str))
 end
 
 local test = tap.test('merger')
-test:plan(6)
-local merger_inst = get_merger_inst()
-check_merger_inst(merger_inst, test, 1)
-check_merger_inst(merger_inst, test, 2)
-check_merger_inst(merger_inst, test, 3)
-check_merger_inst(merger_inst, test, 4)
-check_merger_inst(merger_inst, test, 5)
+test:plan(1 + #schemas * 2 * 6)
 
--- check more buffers then tuples count
-check_merger_inst(merger_inst, test, 1000)
+local ok, err = pcall(merger.new, {{
+    fieldno = 2,
+    type = 'unknown',
+}})
+test:is_deeply({ok, err}, {false, 'Unknown field type: unknown'},
+    'incorrect field type')
+
+for _, use_function_input in ipairs({false, true}) do
+    for _, schema in ipairs(schemas) do
+        local use_function_input_str = use_function_input and
+            ' (use_function_input)' or ''
+        test:diag('testing on schema %s%s', schema.name, use_function_input_str)
+
+        -- Check with small buffers count.
+        local opts = {use_function_input = use_function_input}
+        run_merger(test, schema, 100, 1, opts)
+        run_merger(test, schema, 100, 2, opts)
+        run_merger(test, schema, 100, 3, opts)
+        run_merger(test, schema, 100, 4, opts)
+        run_merger(test, schema, 100, 5, opts)
+
+        -- Check more buffers then tuples count.
+        run_merger(test, schema, 100, 1000, opts)
+    end
+end
 
 -- XXX: test merger_inst:cmp()
 
